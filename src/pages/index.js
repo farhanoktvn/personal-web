a91190a19cd01eea76bb23c8a163a1213a6eb3cd
Farhan Oktavian <farhanoktvn@gmail.com>
2021-03-20 15:55:39 +0000

    installed tailwind

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Muhammad Farhan Oktavian</title>
+      </Head>
+
+      <main>
+
+      </main>
+
+      <footer>
+        
+      </footer>
+    </div>
+  )
+}
